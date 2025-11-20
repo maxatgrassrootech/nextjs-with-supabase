c4d1f90f41b0f06956de8e1365a8efb8e07703c8
Max Li <max@regenerating.us>
2025-11-20 11:53:27 -0500

    footer change.

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
@@ -23,17 +22,7 @@ export default function Home() {
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
+          <p>Created by Max Li for Philly Ministry of CBCGB</p>
           <ThemeSwitcher />
         </footer>
       </div>
