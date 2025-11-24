ce74418245cb511338d768f800ee17c3ed4ceab8
Max Li <max@grassrootech.com>
2025-11-24 17:24:39 -0500

    change metadata.... from starter kit to scheduling manager.

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Philly Ministry Schedule Manager",
+  description: "Max Li created this web app to serve Philly Ministry of CBCGB.",
 };
 
 const geistSans = Geist({
